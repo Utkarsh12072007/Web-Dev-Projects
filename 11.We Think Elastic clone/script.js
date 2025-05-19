2f6184cfe8bb9656a59bcca52c29dd0931451e54
Utkarsh12072007 <patidarutkarsh183@gmail.com>
2025-05-19 17:26:14 +0530

    nav bar

--- 11.We Think Elastic clone/script.js
+++ 11.We Think Elastic clone/script.js
@@ -1,5 +1,9 @@
 var imgs = document.querySelectorAll('.images');
 var intro = document.querySelector('#intro');
+var navi = document.querySelector('#nav');
+var page1 = document.querySelector('#page1');
+var website = document.querySelector('#website');
+var by = document.querySelector('body');
 const tl = gsap.timeline();
 imgs.forEach((a)=>{
     tl.to(a,{opacity:1,duration:0.28})
@@ -12,5 +16,20 @@ tl.to('#intro',{
     ease:"power3.inOut",
     onComplete:()=>{
         intro.style.display = 'none'
+        by.classList.remove('overflow-hidden');
+        by.classList.add('overflow-y-scroll')
     }
+})
+var lastScrollY = window.scrollY;
+window.addEventListener('scroll',()=>{
+    var currentScroll = window.scrollY;
+    if(currentScroll + 0.5 > lastScrollY){
+        navi.classList.remove('opacity-100','translate-y-0')
+        navi.classList.add('opacity-0','-translate-y-full');
+    }
+    else{
+        navi.classList.remove('opacity-0','-translate-y-full');
+        navi.classList.add('opacity-100','translate-y-0')
+    }
+    lastScrollY = currentScroll;
 })
